955ed478ececeb5b4fee5def1d0de7a176b0e302
AlifSakib <alifsakib@gmail.com>
2022-10-17 22:09:49 +0600

    Route setup done

---
+++ src/firebase/firebase.config..js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwCWwT950g7EleKz4H6pMbAdcvn2Agtho",
+  authDomain: "my-news-ee2fd.firebaseapp.com",
+  projectId: "my-news-ee2fd",
+  storageBucket: "my-news-ee2fd.appspot.com",
+  messagingSenderId: "862808917670",
+  appId: "1:862808917670:web:6b9b9c3c0f2303b1b21667",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
